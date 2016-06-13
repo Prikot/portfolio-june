4a9a1ccad36bc15658081cbff97bcd3cc946d325
Prikot <a.prikotov@gmail.com>
2016-06-13 23:55:21 +0800

    added gulp and gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-11
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+
+gulp.task('default', function () {
+  return gulp.src('assets/**/*.css')
+    .pipe(concatCss("styles/bundle.css"))
+    .pipe(gulp.dest('out/'));
+});
+

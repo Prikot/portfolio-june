7cf4e07c4fd7e1cae3f5f6874c65943510f02b6f
Prikot <a.prikotov@gmail.com>
2016-06-20 15:49:21 +0800

    dua halaman

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var concatCss = require('gulp-concat-css');
+var sass = require('gulp-sass');
 
 
 gulp.task('default', function () {
@@ -10,4 +11,26 @@ gulp.task('default', function () {
     .pipe(concatCss("styles/bundle.css"))
     .pipe(gulp.dest('out/'));
 });
+
+
+gulp.task('gulp-jade', function() {
+  var YOUR_LOCALS = {};
+
+  gulp.src('./lib/*.jade')
+      .pipe(jade({
+        locals: YOUR_LOCALS
+      }))
+      .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
 
